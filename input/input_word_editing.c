c35e7cd04ba02c850fd873066b1155389d2b61af
Yagsu <juuttera@student.hive.fi>
2020-08-16 21:09:22 +0300

    Changed moving by word to match behaviour found in readline

--- input/input_word_editing.c
+++ input/input_word_editing.c
@@ -6,7 +6,7 @@
 /*   By: jesse <jesse@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 00:23:30 by jesse             #+#    #+#             */
-/*   Updated: 2020/08/13 22:21:40 by jesse            ###   ########.fr       */
+/*   Updated: 2020/08/16 21:08:28 by jesse            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int		word_border_at_left(struct s_term_config *term)
 	int index;
 
 	index = term->pos;
-	if (index == term->line.len)
-		index--;
-	while (index >= 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index]))
-		index--;
-	if (index >= 0 && term->line.data[index] && ft_iswhitespace(term->line.data[index]))
+	if (index >= 0 && (ft_iswhitespace(term->line.data[index]) || ft_iswhitespace(term->line.data[index - 1]) || index == term->line.len))
 	{
+		index--;
 		while (index >= 0 && term->line.data[index] && ft_iswhitespace(term->line.data[index]))
 			index--;
+	}
+	if (index >= 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index]))
+	{
 		while (index > 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index - 1]))
 			index--;
 		return (index);
@@ -38,12 +38,16 @@ int		word_border_at_right(struct s_term_config *term)
 
 	index = term->pos;
 	if (index == term->line.len)
-		index--;
-	while (index >= 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index]))
-		index++;
-	if (index >= 0 && term->line.data[index] && ft_iswhitespace(term->line.data[index]))
+		return (-1);
+	if (index < term->line.len && (ft_iswhitespace(term->line.data[index]) || ft_iswhitespace(term->line.data[index + 1])))
 	{
+		index++;
 		while (index >= 0 && term->line.data[index] && ft_iswhitespace(term->line.data[index]))
+			index++;
+	}		
+	if (index >= 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index]))
+	{
+		while (index >= 0 && term->line.data[index] && !ft_iswhitespace(term->line.data[index]))
 			index++;
 		return (index);
 	}
